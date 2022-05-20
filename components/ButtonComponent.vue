c1e8d915fa8ef790fbccdf02e8f2f0c2e353c3a2
Sergey Kovalev <sergey@weedydidie.com>
2022-05-20 09:11:27 +0700

    fix: button

--- components/ButtonComponent.vue
+++ components/ButtonComponent.vue
@@ -23,24 +23,24 @@ export default {
   text-transform: none;
   color: #1D1D1D;
   background: linear-gradient(92.05deg, #2DEA40 3.12%, #BFFF37 81.34%);
-  box-shadow: 0px 5px 15px rgba(143, 255, 0, 0.2);
+  box-shadow: 0px 0.208333em 0.625em rgba(143, 255, 0, 0.2);
 
   font-family: 'Museo';
   font-style: normal;
   font-weight: 650;
-  font-size: 24px;
-  line-height: 40px;
+  font-size: 1.5em; //24px;
+  line-height: 2.5em; //40px;
   letter-spacing: normal;
   text-indent: 0;
-  padding: 0.865em 2.188em !important;
-  height: 4.375rem !important;
-  width: 15.625rem !important;
-  border-radius: 2em;
+  // padding: 0.865em 2.188em !important;
+  height: 2.916667em !important;
+  width: 10.416667em !important; // 250px
+  border-radius: 2.916667em;
 
   // margin-top: 3em;
   // margin-bottom: 2em;
 
-  // @include fluid-type(font-size, $minScreen, $maxScreen, 1.125rem, 1.5rem); // 17px - 16px
+  @include fluid-type(font-size, $minScreen, $maxScreen, 1.125em, 1.5em); // 18px - 24px
 
 }
 
