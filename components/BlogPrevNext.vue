c70272c959db51320069496ab76e12ab5324f315
Sergey Kovalev <sergey@weedydidie.com>
2023-04-08 13:51:47 +0100

    feat: artivle navigation for mobile

--- components/BlogPrevNext.vue
+++ components/BlogPrevNext.vue
@@ -63,6 +63,9 @@ export default {
 <style lang="scss" scoped>
 @import '~/assets/globals.scss';
 
+$breakpoint-to-column: 50rem; //800px
+$breakpoint-to-shrink-lines: 87.5rem; //1400px
+
 .blog-navigator-wrapper {
   @include fluid-type(margin-bottom, $minScreen, $maxScreen, 3.75rem, 8.75rem); // 140 - 60
   @include fluid-type(margin-top, $minScreen, $maxScreen, 3.75rem, 4.375rem); // 70 - 60
@@ -70,6 +73,10 @@ export default {
 }
 
 .blog-navigator {
+  @media only screen and (max-width: $breakpoint-to-column) {
+    flex-direction: column;
+    @include fluid-type(row-gap, $minScreen, $maxScreen, 1.875rem, 8.75rem); // 60 - 30
+  }
   display: flex;
   justify-content: space-between;
 }
@@ -81,6 +88,11 @@ export default {
 }
 
 .blog-navigator-button-content {
+  @media only screen and (max-width: $breakpoint-to-column) {
+    flex-wrap: wrap;
+    height:auto;
+    width: unset;
+  }
   display: flex;
   align-items: center;
   width: 35.42vw;
@@ -88,11 +100,21 @@ export default {
 }
 
 .blog-navigator-image-wrapper {
+  @media only screen and (max-width: $breakpoint-to-column) {
+    order: -1;
+    width: 100%;
+    height: unset;
+  }
   width: 26.47%;
   height: 5.73vw;
 }
 
 .blog-navigator-image {
+  @media only screen and (max-width: $breakpoint-to-column) {
+    width: 100%;
+    height: unset;
+    border-radius: 0.625em 0.625em 0 0;
+  }
   width: 100%;
   height: 5.73vw;
   object-fit: cover;
@@ -101,6 +123,9 @@ export default {
 }
 
 .blog-navigator-image.next {
+  @media only screen and (max-width: $breakpoint-to-column) {
+    border-radius: 0.625em 0.625em 0 0;
+  }
   border-radius: 0.625em 0 0 0.625em;
 }
 
@@ -110,11 +135,25 @@ export default {
 }
 
 .blog-navigator-text {
+  @media only screen and (max-width: $breakpoint-to-shrink-lines) {
+    -webkit-line-clamp: 2;
+    line-clamp: 2;
+  }
+
+  @media only screen and (max-width: $breakpoint-to-column) {
+    width: 85%;
+    max-height: unset;
+    padding-top: 1.2em;
+    padding-bottom: 1.2em;
+    -webkit-line-clamp: unset;
+    line-clamp: unset;
+  }
+
   color: #fff;
   padding-right: 1.875em;
-  padding-left: .6em;
-  padding-top: .6em;
-  padding-bottom: .6em;
+  // padding-left: .6em;
+  margin-top: .6em;
+  margin-bottom: .6em;
 
   display: -webkit-box;
   -webkit-line-clamp: 3;
@@ -122,21 +161,27 @@ export default {
   overflow: hidden;
   -webkit-box-orient: vertical;
 
-  width: 27.25rem;
   width: 64.12%;
   max-height: 5.73vw;
 }
 
 .blog-navigator-text.next {
   padding-left: 1.875em;
-  padding-right: .6em;
+  // padding-right: .6em;
 }
 
 .blog-navigator-arrow {
-  // display: flex;
-  // justify-content: center;
-  padding-left: 1.64em;
-  padding-right: .6em;
+  @media only screen and (max-width: $breakpoint-to-column) {
+    width: 15%;
+    display: flex;
+    justify-content: center;
+    padding: unset;
+  }
+  display: flex;
+    justify-content: center;
+
+  // padding-left: 1.64em;
+  // padding-right: .6em;
   width: 9.41%;
 
   img {
@@ -148,6 +193,9 @@ export default {
   }
 }
 .blog-navigator-arrow.next {
+  @media only screen and (max-width: $breakpoint-to-column) {
+    padding: unset;
+  }
   padding-right: 1.64em;
   padding-left: .6em;
 }
